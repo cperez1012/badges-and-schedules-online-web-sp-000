15ed94e5722566527590a6fa7dd44f8ef506c147
Christian Perez <perezchristian1012@gmail.com>
2019-07-24 19:14:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ def assign_rooms(attendees)
     "Hello, #{attendees}! You'll be assigned to room #{index + 1}!"  }
 end
 
-def printer
+def printer(attendees)
   batch_badge_creator(attendees).each {|badge|
   puts badge}
   assign_rooms(attendees).each {|room|
