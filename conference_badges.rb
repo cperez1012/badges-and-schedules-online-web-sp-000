7daa6b990b56fdde570ff529daefbab67df156da
Christian Perez <perezchristian1012@gmail.com>
2019-07-24 18:44:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,12 @@
 # Write your code here.
-def badge_maker
+def badge_maker(name)
+  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(attendees)
+  attendees = []
+  attendees.each do |attendees|
+  "Hello, my name is #{attendees}."
 end
 
 def assign_rooms
